03a454241ba6f9b91701b5af0e6647702673dc6c
Alen Jo <jo.alen1@outlook.com>
2026-04-08 16:50:41 -0500

    Program Binary Generation
    - now when you go ahead and write up some Haskell code, it should compile down to LLVM IR and run as a binary executable
     - we did it! E2E flow of my compiler (even if it took a year to make)

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ pub mod parser;
 pub mod llvmgen;
 
 use inkwell::context::Context;
+use inkwell::targets::{
+    CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine,
+};
+use inkwell::OptimizationLevel;
+
 use lexer::Lexer;
 use parser::{Parser, Precedence};
 use ast::types::TypeInference;
@@ -80,4 +85,83 @@ pub fn compile_program(source: &str) -> Result<String, String> {
     }
 
     Ok(cg.module.print_to_string().to_string())
+}
+
+pub fn compile_to_binary(source: &str, output_path: &str) -> Result<(), String> {
+    // parsing
+    let lexer = Lexer::new(source);
+    let mut parser = Parser::new(lexer);
+    let mut decls = Vec::new();
+    while parser.current_token != Token::Eof {
+        if parser.current_token == Token::VirtualSemi {
+            parser.next_token();
+            continue;
+        }
+        match parser.parse_decl() {
+            Some(decl) => decls.push(decl),
+            None => break,
+        }
+    }
+
+    // type checking
+    let mut ti = TypeInference::new();
+    for decl in &decls {
+        if let Decl::FuncDef(name, params, _) = decl {
+            let ret_ty = ti.type_var_gen.fresh();
+            let ty = params.iter().rfold(ret_ty, |acc, _| {
+                let param_ty = ti.type_var_gen.fresh();
+                Type::Function(Box::new(param_ty), Box::new(acc))
+            });
+            ti.env.insert(name.clone(), TypeScheme {
+                type_vars: vec![],
+                constraints: vec![],
+                ty,
+            });
+        }
+    }
+    for decl in &decls {
+        ti.register_decl(decl);
+    }
+
+    // codegen
+    let context = Context::create();
+    let mut cg = Codegen::new(&context);
+    for decl in &decls {
+        cg.compile_decl(decl)?;
+    }
+
+    // native target init
+    Target::initialize_native(&InitializationConfig::default())
+        .map_err(|e| e.to_string())?;
+
+    let triple = TargetMachine::get_default_triple();
+    let target = Target::from_triple(&triple)
+        .map_err(|e| e.to_string())?;
+    let machine = target.create_target_machine(
+        &triple,
+        "generic",
+        "",
+        OptimizationLevel::Default,
+        RelocMode::Default,
+        CodeModel::Default,
+    ).ok_or("Failed to create target machine")?;
+
+    // write object file
+    let obj_path = format!("{}.o", output_path);
+    machine.write_to_file(&cg.module, FileType::Object, obj_path.as_ref())
+        .map_err(|e| e.to_string())?;
+
+    // system linker linking
+    let status = std::process::Command::new("cc")
+        .args([&obj_path, "-o", output_path])
+        .status()
+        .map_err(|e| e.to_string())?;
+
+    if !status.success() {
+        return Err("Linking failed".into());
+    }
+
+    // remove object file
+    std::fs::remove_file(&obj_path).ok();
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,97 +3,14 @@ mod parser;
 mod llvmgen;
 mod ast; 
 
-use std::collections::HashMap;
-
-use inkwell::context::Context;
-use crate::llvmgen::Codegen;
-use crate::ast::types::{ClassDef, Instance, TypeInference};
-use crate::ast::ast::{BinaryOp, Expr, Type}; 
-
 fn main() {
-    let context = Context::create();
-    let mut cg = Codegen::new(&context);
+    let source = "
+        fact n = if n == 0 then 1 else n * fact (n - 1)
+        main = fact 10
+    ";
 
-    // Test 1: simple int
-    let expr = Expr::Int(42);
-    cg.compile_module(&expr).unwrap();
-    println!("{}", cg.module.print_to_string().to_string());
-
-    let mut type_infer = TypeInference::new();
-    let expr = Expr::Float(42.0);
-
-    // check for a simple number
-    match type_infer.infer(&expr) {
-        Ok((ty, constraints)) => {
-            if constraints.is_empty() {
-                println!("Inferred type: {:?}", ty);
-            } else {
-                println!("Inferred type: {:?} with constraints {:?}", ty, constraints);
-            }
-        }
-        Err(err) => eprintln!("Type inference error: {}", err),
+    match nexity::compile_to_binary(source, "output") {
+        Ok(()) => println!("Compiled to ./output"),
+        Err(e) => eprintln!("Error: {}", e),
     }
-
-    // identity lambda 
-    let id_expr = Expr::Lambda(
-        vec!["x".to_string()],
-        Box::new(Expr::Identifier("x".to_string()))
-    );
-
-    // constant lambda
-    let const_expr = Expr::Lambda( 
-        vec!["x".to_string(), "y".to_string()],
-        Box::new(Expr::Int(42))
-    );
-
-    let id_type = type_infer.infer(&id_expr).unwrap();
-    let const_type = type_infer.infer(&const_expr).unwrap();
-
-    println!("id_expr type: {:?}", id_type);
-    println!("const_expr type: {:?}", const_type);
-
-    let annotated = Expr::Annotated(
-        Box::new(Expr::Int(42)),
-        Type::Int
-    );
-    let result = type_infer.infer(&annotated).unwrap();
-    println!("Annotated type: {:?}", result);
-
-        // Register Eq class
-        type_infer.class_env.classes.insert(
-            "Eq".to_string(),
-            ClassDef {
-                name: "Eq".to_string(),
-                param: "a".to_string(),
-                methods: HashMap::from([
-                    ("==".to_string(), Type::Function(
-                        Box::new(Type::TypeVar("a".to_string())),
-                        Box::new(Type::Function(
-                            Box::new(Type::TypeVar("a".to_string())),
-                            Box::new(Type::Bool)
-                        ))
-                    ))
-                ]),
-            }
-        );
-        
-        // Register instance Eq Int
-        type_infer.class_env.instances.push(Instance {
-            class_name: "Eq".to_string(),
-            ty: Type::Int,
-            methods: HashMap::new(),
-        });
-        
-        // Test: (\x y -> x == y) :: Eq a => a -> a -> Bool
-        let eq_lambda = Expr::Lambda(
-            vec!["x".to_string(), "y".to_string()],
-            Box::new(Expr::BinaryOp(
-                Box::new(Expr::Identifier("x".to_string())),
-                BinaryOp::Equal,
-                Box::new(Expr::Identifier("y".to_string()))
-            ))
-        );
-        
-        let result = type_infer.infer(&eq_lambda).unwrap();
-        println!("Eq lambda type: {:?}", result);
 }
